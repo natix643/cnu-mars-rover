d0b09bc6afbe867c3c986d71daa0e405b8a32383
Adam Kučera <kucera@cngroup.dk>
2017-10-04 10:45:46 +0100

    Initial project skeleton

---
+++ src/test/groovy/dk/cngroup/university/MainTest.groovy
@@ -0,0 +1,18 @@
+package dk.cngroup.university
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class MainTest extends Specification {
+
+    @Unroll
+    "application should start"() {
+        when:
+        Main.main()
+
+        then:
+        noExceptionThrown()
+    }
+
+}
